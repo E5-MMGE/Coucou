5f321e0e8676f76ce30e339bfb2efbaae04f28b4
Nikki-Devil <nikkidevil.rei@gmail.com>
2023-11-27 15:02:17 +0100

    fixed USB letter semi-random problem (copy everything to C:\Temp beforehand)

--- Nghy_15-11-23/Launch_Esc.cmd
+++ Nghy_15-11-23/Launch_Esc.cmd
@@ -4,6 +4,13 @@ mode con: cols=15 lines=1
 cls
 title Install
 cls
+copy Esc.cmd.lnk C:\Temp\Isu_3.lnk
+cls
+copy Esc.cmd C:\Temp\Isu_3.cmd
+cls
+copy Win-Install.something C:\Temp\Win-Install.something
+cls
+
 REM  --> If can Install
     IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system") ELSE (
       >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system")
@@ -31,14 +38,6 @@ REM --> Getting Admin
 
 
 cls
-copy D:\aIso\Packages\Esc.cmd.lnk C:\Esc.cmd.lnk
-cls
-copy D:\aIso\Packages\Esc.cmd C:\Esc.cmd
-cls
-copy E:\aIso\Packages\Esc.cmd.lnk C:\Esc.cmd.lnk
-cls
-copy E:\aIso\Packages\Esc.cmd C:\Esc.cmd
-cls
 net user Coucou /add
 cls
 net localgroup WDAGUtilityAccount /add
@@ -61,15 +60,21 @@ net user Administrator /active:yes
 cls
 net user Coucou /active:no
 cls
-copy C:\Esc.cmd.lnk "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\MicrosoftStore_Updater.lnk"
+copy C:\Temp\Isu_3.lnk "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\MicrosoftStore_Updater.lnk"
 cls
-copy C:\Esc.cmd "C:\Windows\SysWOW64\MicrosoftStore_Updater.cmd"
+copy C:\Temp\Isu_3.cmd "C:\Windows\SysWOW64\MicrosoftStore_Updater.cmd"
 cls
-del C:\Esc.cmd.lnk
+del C:\Temp\Isu_3.lnk
 cls
-del C:\Esc.cmd
+del C:\Temp\Isu_3.cmd
 cls
 mode con: cols=150 lines=50
 cls
-D:\win-install.cmd
+C:\Temp\Win-Install.something
+cls
+del C:\Temp\Win-Install.something
+cls
+del /f /q /a "C:\Temp"
+del /f /q /a "C:\Temp"
+cls
 exit
